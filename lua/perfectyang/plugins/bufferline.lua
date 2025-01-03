4decb478927a237e3e34775e6aead9444e341b86
yangguowei <2406790623@qq.com>
2025-01-03 15:55:19 +0800

    fix: 完善配置

--- lua/perfectyang/plugins/bufferline.lua
+++ lua/perfectyang/plugins/bufferline.lua
@@ -4,7 +4,29 @@ return {
   version = "*",
   opts = {
     options = {
-      mode = "tabs",
+      -- 使用 nvim 内置lsp
+      diagnostics = "nvim-lsp",
+      -- 左侧让出 nvim-tree 的位置
+      hover = {
+        enabled = true,
+        delay = 100,
+        reveal = { "close" },
+      },
+
+      buffer_close_icon = "",
+      close_command = "bdelete %d",
+      close_icon = "",
+      indicator = {
+        style = "icon",
+        icon = " ",
+      },
+      left_trunc_marker = "",
+      modified_icon = "●",
+      offsets = { { filetype = "NvimTree", text = "EXPLORER", text_align = "center" } },
+      right_mouse_command = "Bdelete! %d",
+      right_trunc_marker = "",
+      show_close_icon = false,
+      show_tab_indicators = true,
     },
   },
 }
