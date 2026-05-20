79b68c4651f68f7eb2f53ecdcc234671a7f341f3
perfectyang <perfectyang@perfecytang.local>
2026-05-20 15:56:06 +0800

    fix:  完善配置

--- lua/perfectyang/plugins/coc.lua
+++ lua/perfectyang/plugins/coc.lua
@@ -1,3 +1,4 @@
 return {
   "neoclide/coc.nvim",
+  enabled = false,
 }
--- lua/perfectyang/plugins/lsp/mason.lua
+++ lua/perfectyang/plugins/lsp/mason.lua
@@ -1,6 +1,7 @@
 return {
   "williamboman/mason.nvim",
   dependencies = {
+    "neovim/nvim-lspconfig",
     "williamboman/mason-lspconfig.nvim",
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     "hrsh7th/cmp-nvim-lsp",
@@ -23,102 +24,122 @@ return {
     local cmp_nvim_lsp = require("cmp_nvim_lsp")
     local capabilities = cmp_nvim_lsp.default_capabilities()
 
+    vim.diagnostic.config({
+      virtual_text = {
+        spacing = 2,
+        source = "if_many",
+      },
+      signs = true,
+      underline = true,
+      update_in_insert = false,
+      severity_sort = true,
+      float = {
+        border = "rounded",
+        source = "if_many",
+      },
+    })
+
     local on_attach = function(_, bufnr)
-      -- local nmap = function(keys, func, desc)
-      --   vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-      -- end
-      --
+      local nmap = function(keys, func, desc)
+        vim.keymap.set("n", keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
+      end
+
       -- nmap("K", vim.lsp.buf.hover, "Hover")
-      -- nmap("<leader>rn", vim.lsp.buf.rename, "Rename")
-      -- nmap("<leader>ca", vim.lsp.buf.code_action, "Code Action")
-      -- nmap("gd", vim.lsp.buf.definition, "Goto Definition")
-      -- nmap("gD", vim.lsp.buf.declaration, "Goto Declaration")
-      -- nmap("gi", vim.lsp.buf.implementation, "Goto Implementation")
-      -- nmap("gr", require("telescope.builtin").lsp_references, "References")
+      -- nmap("gD", vim.lsp.buf.declaration, "Go to declaration")
+      -- nmap("gi", vim.lsp.buf.implementation, "Go to implementation")
+      -- nmap("<leaner>rn", vim.lsp.buf.rename, "Rename")
+      -- nmap("<leader>ca", vim.lsp.buf.code_action, "Code action")
+      -- nmap("<leader>e", vim.diagnostic.open_float, "Line diagnostics")
+      --
+      -- local ok, builtin = pcall(require, "telescope.builtin")
+      -- if ok then
+      --   nmap("gr", builtin.lsp_references, "References")
+      --   nmap("gd", builtin.lsp_definitions, "Go to definition")
+      --   nmap("gt", builtin.lsp_type_definitions, "Go to type definition")
+      -- else
+      --   nmap("gr", vim.lsp.buf.references, "References")
+      --   nmap("gd", vim.lsp.buf.definition, "Go to definition")
+      --   nmap("gt", vim.lsp.buf.type_definition, "Go to type definition")
+      -- end
     end
 
-    vim.lsp.config.bashls = {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "bash", "sh", "zsh" },
+    local servers = {
+      bashls = {
+        filetypes = { "bash", "sh", "zsh" },
+      },
+      html = {
+        filetypes = { "html" },
+      },
+      cssls = {
+        filetypes = { "css", "scss", "less" },
+      },
+      tailwindcss = {
+        filetypes = {
+          "html",
+          "css",
+          "scss",
+          "less",
+          "javascriptreact",
+          "typescriptreact",
+          "svelte",
+        },
+      },
+      svelte = {
+        filetypes = { "svelte" },
+      },
+      lua_ls = {
+        filetypes = { "lua" },
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { "vim" },
+            },
+            workspace = {
+              checkThirdParty = false,
+            },
+            telemetry = {
+              enable = false,
+            },
+          },
+        },
+      },
+      graphql = {
+        filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+      },
+      emmet_ls = {
+        filetypes = {
+          "html",
+          "css",
+          "scss",
+          "less",
+          "javascriptreact",
+          "typescriptreact",
+          "svelte",
+        },
+      },
+      prismals = {
+        filetypes = { "prisma" },
+      },
+      pyright = {
+        filetypes = { "python" },
+      },
+      ts_ls = {
+        filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact" },
+        root_markers = { "package.json", "tsconfig.json", "jsconfig.json", ".git" },
+      },
     }
 
-    vim.lsp.config.html = {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "html" },
-    }
-
-    vim.lsp.config.cssls = {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "css", "scss", "less" },
-    }
-
-    vim.lsp.config.tailwindcss = {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "html", "css", "scss", "less" },
-    }
-
-    vim.lsp.config.svelte = {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "svelte" },
-    }
-
-    vim.lsp.config.lua_ls = {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "lua" },
-    }
-
-    vim.lsp.config.graphql = {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "graphql" },
-    }
-
-    vim.lsp.config.emmet_ls = {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "html", "javascriptreact", "typescriptreact" },
-    }
-
-    vim.lsp.config.prismals = {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "prisma" },
-    }
-
-    vim.lsp.config.pyright = {
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "python" },
-    }
-
-    vim.lsp.config.ts_ls = {
-      cmd = { "vtsls", "--stdio" }, -- 启动服务器的命令[reference:0]
-      capabilities = capabilities,
-      on_attach = on_attach,
-      filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact" },
-      root_markers = { "package.json", "tsconfig.json", ".git" }, -- 用于确定项目根目录的标志文件[reference:2]
-    }
-
-    vim.lsp.enable("bashls")
-    vim.lsp.enable("html")
-    vim.lsp.enable("cssls")
-    vim.lsp.enable("tailwindcss")
-    vim.lsp.enable("svelte")
-    vim.lsp.enable("lua_ls")
-    vim.lsp.enable("graphql")
-    vim.lsp.enable("emmet_ls")
-    vim.lsp.enable("prismals")
-    vim.lsp.enable("pyright")
-    vim.lsp.enable("ts_ls")
+    for server, opts in pairs(servers) do
+      vim.lsp.config[server] = vim.tbl_deep_extend("force", {
+        capabilities = capabilities,
+        on_attach = on_attach,
+      }, opts)
+      vim.lsp.enable(server)
+    end
 
     mason_lspconfig.setup({
       ensure_installed = {
+        "bashls",
         "html",
         "cssls",
         "tailwindcss",
@@ -130,6 +151,7 @@ return {
         "pyright",
         "ts_ls",
       },
+      automatic_enable = false,
     })
 
     mason_tool_installer.setup({
